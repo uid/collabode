59603a979d6c23b5725940ffc47886adc3faf789
Max Goldman <maxg@mit.edu>
2011-10-24 18:27:13 -0400

    Fix whitespace

--- src/control/clone_control.js
+++ src/control/clone_control.js
@@ -7,7 +7,7 @@ import("editor.workspace");
 jimport("collabode.Workspace");
 
 jimport("java.lang.System");
-         
+
 function clone_path(projectname, filename) {
   var destination = projectname+"-"+getSession().userName;
   var project = Workspace.cloneProject(getSession().userId, projectname, destination);
@@ -18,7 +18,7 @@ function clone_path(projectname, filename) {
       auth.del_acl(project, acl.path, acl.userId);
       if ((acl.userId == auth.ANYONE) || (acl.userId == getSession().userId)) {
         auth.add_acl(project, acl.path, getSession().userId, auth.OWNER);
-      }      
+      }
     }
   });
   
