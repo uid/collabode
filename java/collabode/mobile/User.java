7b9c711a617d1e29de510f5abf9f70f25a26149a
Angela N. Chang <nihwey.chang@gmail.com>
2012-01-13 20:07:25 -0500

    Just some tab/spacing changes

--- java/collabode/mobile/User.java
+++ java/collabode/mobile/User.java
@@ -1,12 +1,11 @@
 package collabode.mobile;
 
 public class User {
-
-	public String username;
-	public String photo;
-	
-	public User(String username, String photo) {
-		this.username = username;
-		this.photo = photo;
-	}
+    public String username;
+    public String photo;
+    
+    public User(String username, String photo) {
+        this.username = username;
+        this.photo = photo;
+    }
 }
