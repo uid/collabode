bb2269ca9a524915cde2693515ba0ba217ae431e
Robin Cheng <hpmv118@gmail.com>
2012-04-29 00:20:41 -0400

    Fix for IE 8.
    The bug is due to IE 8 not having Array.forEach.

--- src/static/js/tree_client.js
+++ src/static/js/tree_client.js
@@ -1,3 +1,33 @@
+// Fix for IE 8 (It doesn't have Array.forEach)
+// Production steps of ECMA-262, Edition 5, 15.4.4.18
+// Reference: http://es5.github.com/#x15.4.4.18
+// Original comments removed.
+if (!Array.prototype.forEach) {
+  Array.prototype.forEach = function(callback, thisArg) {
+    var T, k;
+    if (this == null) {
+      throw new TypeError(" this is null or not defined");
+    }
+    var O = Object(this);
+    var len = O.length >>> 0;
+    if ({}.toString.call(callback) != "[object Function]") {
+      throw new TypeError(callback + " is not a function");
+    }
+    if (thisArg) {
+      T = thisArg;
+    }
+    k = 0;
+    while (k < len) {
+      var kValue;
+      if (k in O) {
+        kValue = O[k];
+        callback.call(T, kValue, k, O);
+      }
+      k++;
+    }
+  };
+}
+
 function getTreeClient(comet) {
   $(document).ready(function() {
 
