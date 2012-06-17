ab184f2959ae8d45b4f79b339f6c163caf3c08e7
Max Goldman <maxg@mit.edu>
2012-06-17 19:38:06 -0400

    Show filenames in contrib and coverage views
    Fixes issue #69 with contrib, and fixes part of issue #71 with coverage.

--- src/static/js/ode_test.js
+++ src/static/js/ode_test.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
+  function title(name) {
+    return name.substring(name.lastIndexOf('/')+1);
+  }
+  
   var testRep = clientVars.files[clientVars.testfilename];
-  var testDiv = Readonly.appendHTML('#test', testRep);
+  var testDiv = Readonly.appendHTML('#test', testRep, title(clientVars.testfilename));
   $.each(testRep.unfolded, function(idx, lines) {
     testDiv.children('.ace-line').slice(lines.from, lines.to+1).addClass('unfaded');
   });
   delete clientVars.files[clientVars.testfilename];
   
   $.each(clientVars.files, function(name, rep) {
-    var div = Readonly.appendHTML('#files', rep);
+    var div = Readonly.appendHTML('#files', rep, title(name));
     $.each(rep.unfolded, function(idx, lines) {
       div.children('.ace-line').slice(lines.from, lines.to+1).addClass('unfaded');
     });
